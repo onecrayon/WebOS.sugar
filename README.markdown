3097a500177b7e3c4587d3c929bd2bd4d4185839
Ian Beck <ian@onecrayon.com>
2010-04-11 08:47:19 -0700

    Added basic sugar info to meta files
    Finished tweaking shell script invocation for creating a new app

---
+++ README.markdown
@@ -0,0 +1,18 @@
+WebOS.sugar for Espresso
+========================
+
+The WebOS.sugar provides actions and snippets that make
+programming applications for Palm's WebOS easier in the
+[Espresso text editor][esp] for Mac.
+
+For more information, documentation, and download:
+
+<http://onecrayon.com/products/webos-sugar/>
+
+If you have any requests or stumble across any bugs,
+please feel free to [file an issue][issues] or [email
+me][contact].
+
+   [esp]: http://macrabbit.com/espresso/
+   [issues]: http://github.com/onecrayon/WebOS.sugar/issues
+   [contact]: http://onecrayon.com/about/contact/
--- Support/Scripts/generate_app.js
+++ Support/Scripts/generate_app.js
@@ -4,15 +4,24 @@ var utils = require('webos-utils');
 
 exports.main = function() {
 	var root = '';
-	// Since the bash profile isn't loaded for the user, make sure we have the WebOS SDK executables in the path
+	// Setup the shared root directory (allows setting up app in sub-folder)
 	root = String(context.contextDirectoryForSelectedResources);
 	if (root.lastIndexOf('/') == root.length - 1) {
 		root = substr(root.length - 2);
 	}
+	// Parse out file://localhost and any space placeholders
 	root = root.replace(/file:\/\/(?:localhost)?(.+)/i, '$1');
 	root = root.replace(/%20/, '\\ ');
+	// We'll use the folder name for the default project name, so grab it
 	var finalFolder = root.substr(root.lastIndexOf('/') + 1);
+	// Since the bash profile isn't loaded for the user, make sure we have the WebOS SDK executables in the path using our utility function
 	var command = utils.prepCommand('palm-generate -p "{title:\'' + finalFolder + '\', id:com.mydomain.' + finalFolder.toLowerCase() + ', version:\'1.0.0\'}" ' + root);
-	system.log(system.bash(command));
-	return true;
+	// Might want to add better error handling here
+	var result = system.shell(command);
+	if (!result) {
+		return false;
+	} else {
+		system.log(result);
+		return true;
+	}
 }
