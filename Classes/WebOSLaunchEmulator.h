58cb97db572aaafeaa91eba48aaf27e787cb2fcf
Ian Beck <ian@onecrayon.com>
2011-08-05 07:30:48 -0700

    Added LaunchEmulator action

---
+++ Classes/WebOSLaunchEmulator.h
@@ -0,0 +1,13 @@
+//
+//  WebOSLaunchEmulator.h
+//  WebOS
+//
+//  Created by Ian Beck on 8/5/11.
+//  Copyright 2011 One Crayon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WebOSLaunchEmulator : NSObject
+
+@end
---
+++ Classes/WebOSLaunchEmulator.m
@@ -0,0 +1,28 @@
+//
+//  WebOSLaunchEmulator.m
+//  WebOS
+//
+//  Created by Ian Beck on 8/5/11.
+//  Copyright 2011 One Crayon. All rights reserved.
+//
+
+#import "WebOSLaunchEmulator.h"
+
+@implementation WebOSLaunchEmulator
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (BOOL)performActionWithContext:(id)context error:(NSError **)outError
+{
+    
+}
+
+@end
