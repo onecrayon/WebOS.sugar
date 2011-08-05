e4cb33a21a1bce74c749ddeb0c3a7d7d9d1f7d43
Ian Beck <ian@onecrayon.com>
2011-08-05 08:16:03 -0700

    Moved action titles into Localizable.strings Added skeleton PackageAndInstall action class

---
+++ Classes/WebOSPackageAndInstall.h
@@ -0,0 +1,13 @@
+//
+//  WebOSPackageAndInstall.h
+//  WebOS
+//
+//  Created by Ian Beck on 8/5/11.
+//  Copyright 2011 One Crayon. All rights reserved.
+//
+
+#import "WebOSGenericAction.h"
+
+@interface WebOSPackageAndInstall : WebOSGenericAction
+
+@end
---
+++ Classes/WebOSPackageAndInstall.m
@@ -0,0 +1,23 @@
+//
+//  WebOSPackageAndInstall.m
+//  WebOS
+//
+//  Created by Ian Beck on 8/5/11.
+//  Copyright 2011 One Crayon. All rights reserved.
+//
+
+#import "WebOSPackageAndInstall.h"
+
+@implementation WebOSPackageAndInstall
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
